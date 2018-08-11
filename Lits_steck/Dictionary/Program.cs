3ff737b52e01c18ed2e341c64842fad85bb454a5
AnonPrimo <esichkaisi@gmail.com>
2018-08-11 14:45:23 +0300

    ver3

--- Lits_steck/Dictionary/Program.cs
+++ Lits_steck/Dictionary/Program.cs
@@ -22,50 +22,76 @@ namespace Dictionary
     {
         static void Main(string[] args)
         {
-           // Dictionary<string, Dictionary<string, List<string>>> timetable = new Dictionary<string, Dictionary<string, List<string>>>();
+            // Dictionary<string, Dictionary<string, List<string>>> timetable = new Dictionary<string, Dictionary<string, List<string>>>();
 
-            Dictionary<string, string> subjects = new Dictionary<string, string>();
+            /* Dictionary<Subjects, string> subjects = new Dictionary<Subjects, string>();
 
-            List<Subjects> subj = new List<Subjects>();
-            subj.Add(new Subjects("History"));
+             List<Subjects> subj = new List<Subjects>();
+             subj.Add(new Subjects("History"));
+
+
+             Days day = new Days();
+
+             // subjects[subj[0]].Add(day.Mon);
+
+             subjects.Add(subj[0], day.Wed);
+
+             foreach (KeyValuePair<Subjects, string> keyValue in subjects)
+             {
+                 Console.WriteLine(keyValue.Key + " - " + keyValue.Value);
+             }*/
+
+            Dictionary<string, List<string>> subjects = new Dictionary<string, List<string>>();
+
+            List<string> subj = new List<string>();
+            subj.Add("History");
 
 
             Days day = new Days();
 
             // subjects[subj[0]].Add(day.Mon);
 
-            subjects.Add(subj[0].ToString(), day.Wed);
+            subjects.Add(subj[0], new List<string>());
 
-            foreach (KeyValuePair<string, string> keyValue in subjects)
+            subjects[subj[0]].Add(day.Mon);
+            subjects[subj[0]].Add(day.Wed);
+            subjects[subj[0]].Add(day.Fry);
+
+
+            foreach (KeyValuePair<string, List<string>> keyValue in subjects)
             {
-                Console.WriteLine(keyValue.Key + " - " + keyValue.Value);
+                for(int i = 0; i<subjects.Values.Count; ++i)
+                Console.WriteLine(keyValue.Key + " - " + keyValue.Value[i]);
             }
+            Console.WriteLine();
+            for(int i = 0; i < subjects.Values.Count; ++i)
+            Console.WriteLine(subjects.Keys + " - " + subjects.Values);
 
-           //Console.WriteLine(subjects.Keys + " - " + subjects.Values);
+            //Console.WriteLine(subjects.Keys + " - " + subjects.Values);
 
-           /* subjects["History"].Add(day.Tue);
-            subjects["History"].Add(day.Fry);
+            /* subjects["History"].Add(day.Tue);
+             subjects["History"].Add(day.Fry);
 
-            subjects["Math"].Add(day.Mon);
-            subjects["Math"].Add(day.Fry);
+             subjects["Math"].Add(day.Mon);
+             subjects["Math"].Add(day.Fry);
 
-            subjects["Programing"].Add(day.Tue);
-            subjects["Programing"].Add(day.Thur);
+             subjects["Programing"].Add(day.Tue);
+             subjects["Programing"].Add(day.Thur);
 
-            subjects["Physics"].Add(day.Wed);
-            subjects["Physics"].Add(day.Thur);
+             subjects["Physics"].Add(day.Wed);
+             subjects["Physics"].Add(day.Thur);
 
-            subjects["English"].Add(day.Tue);
-            subjects["English"].Add(day.Wed);
+             subjects["English"].Add(day.Tue);
+             subjects["English"].Add(day.Wed);
 
-            subjects["Electronics"].Add(day.Mon);
-            subjects["Electronics"].Add(day.Wed);
+             subjects["Electronics"].Add(day.Mon);
+             subjects["Electronics"].Add(day.Wed);
 
-            timetable.Add("DS-214", subjects);
+             timetable.Add("DS-214", subjects);
 
-            timetable.Add("SD-124", subjects);
+             timetable.Add("SD-124", subjects);
 
-            Console.WriteLine(timetable);*/
+             Console.WriteLine(timetable);*/
 
             Console.ReadLine();
         }
